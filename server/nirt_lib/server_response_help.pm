3da06561e69e91b4d75db827c51791d09a5cf16c
MeiChubbs <shelbympeterson@gmail.com>
2015-11-06 14:58:39 -0700

    Update server_response_help.pm
    fixed spelling errors

--- server/nirt_lib/server_response_help.pm
+++ server/nirt_lib/server_response_help.pm
@@ -5,11 +5,11 @@ package server_response_help;
 #Subroutine to process Help
 sub display_help{
 	my $reply =	"\tcommand [string -args] - Send a command output to the logging system.\n".
-			"\tcomment [delimiter] - Send a comment to the loggins system.\n".
+			"\tcomment [delimiter] - Send a comment to the login system.\n".
 			"\tdevice [string] - Set the victim's device name (working directory).\n".
 			"\texit - Stop or suspend the investigation.\n".
-			"\tfile [string] - Senf a file to the logging system.\n".
-			"\thelp - Your looking at it.\n".
+			"\tfile [string] - Send a file to the logging system.\n".
+			"\thelp - You're looking at it.\n".
 			"\tcolor [on/off] - Turn the color on or off.\n".
 			"\tuser [username] - Change the username that is displayed in the log file (currently ".server_response_user::get_user().").\n".
 			"\tvictim [".server_response_victim::get_os_options()."] - Change the OS of the victim's computer (currently ".server_response_victim::get_os().").\n";
